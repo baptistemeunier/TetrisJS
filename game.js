6bbb2dc598631e14477bfbc7f05ca67d629c9237
FindYourSelf <baptiste.meunier0@gmail.com>
2015-10-22 17:56:48 +0200

    Ajout des includes json et objet

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
-var tetros = [[[[0, 1, 2, 3]],[[0],[0],[0],[0]],[[0, 1, 2, 3]],[[0], [0], [0], [0]]], [[[0, 1], [0, 1] ], [[0, 1], [0, 1] ], [[0, 1], [0, 1] ], [[0, 1], [0, 1] ] ], [[[1], [0, 1, 2] ], [[0], [0, 1], [0] ], [[1], [0, 1], [1] ], [[1], [0, 1], [1] ] ], [[[2], [0, 1, 2]], [[0], [0], [0, 1]], [[0, 1, 2], [0]], [[0, 1], [1], [1]] ], [[[0], [0, 1, 2] ], [[0, 1], [0], [0] ], [[0, 1, 2], [2] ], [[1], [1], [0, 1] ] ], [[[0, 1], [1, 2] ], [[1], [0, 1], [0] ], [[0, 1], [1, 2] ], [[1], [0, 1], [0] ] ], [[[1, 2], [0, 1] ], [[0], [0, 1], [1] ], [[1, 2], [0, 1] ], [[0], [0, 1], [1] ] ] ];
+var tetros = JSON.parse(readFile("data/tetrominos.json"));
 var color  = [ "cyan", "yellow", "purple", "orange", "blue", "red", "lime"];
 var c = 30;   // Coté d'un block en pixel 
 var bord =20; // Position du bord gauche en pixel
 var bord_next = 3*bord+10*c; // Position du bord gauche cadre next
-
+includeFile("blockObjet.js");
 document.onkeyup = function(event){ 
   switch(event.keyCode){
       case 16:
@@ -32,129 +32,6 @@ document.onkeyup = function(event){
     }
 };
 
-/** Creation de l'objet Block
- *   Crée le tetromino qui est utilisé dans le jeu
- *   
-**/function Block(type, x, y){
-  this.type     = type;
-  this.rotation = 0;
-  this.coord    = tetros[type][0];
-  this.color    = color[type];
-  this.x        = 0;
-  this.y        = 0;
-  Ecrire(this.type);
-  Ecrire(this.coord);  
-  Ecrire("X : " + this.x);
-  Ecrire("Y : " + this.y);
-  this.draw();
-}
-
-/** Methode Draw de l'objet Block
- *   Permet de crée le tetromino dans la grille
- *   @return void
-**/
-Block.prototype.draw = function(){
-  for(var i=0;i<Taille(this.coord);i++){
-    for(var j=0;j<Taille(this.coord[i]);j++){
-      grid[i+this.x][this.coord[i][j]+this.y][0] = 2;
-      grid[i+this.x][this.coord[i][j]+this.y][1] = this.color;
-    }
-  }
-  for(var i=0;i<Taille(grid);i++){
-    for(var j=0;j<Taille(grid[i]);j++){
-      if(grid[i][j][0]){
-          RectanglePlein(bord+j*c, bord+i*c, c, c, this.color);    
-      }
-    }
-  }
-};
-
-/** Methode Draw de l'objet Block
- *   Permet de crée le tetromino dans la grille
- *   @return void
-**/
-Block.prototype.fixe = function(){
-  for(var i=0;i<Taille(this.coord);i++){
-    for(var j=0;j<Taille(this.coord[i]);j++){
-      grid[i+this.x][this.coord[i][j]+this.y][0] = true;
-  }
-  }
-  for(var i=0;i<Taille(grid);i++){
-    for(var j=0;j<Taille(grid[i]);j++){
-      if(grid[i][j][0]){
-          RectanglePlein(bord+j*c, bord+i*c, c, c, this.color);    
-      }
-    }
-  }
-};
-
-/** Methode Remove de l'objet Block
- *   Permet d'effaser le tetromino de la grille
- *   @return void
-**/
-Block.prototype.remove = function(){
-  for(var i=0;i<Taille(grid);i++){
-    for(var j=0;j<Taille(grid[i]);j++){
-      if(grid[i][j][0]){
-          RectanglePlein(bord+j*c, bord+i*c, c, c, "white");    
-      }
-    }
-  }
-  for(var i=0;i<Taille(this.coord);i++){
-    for(var j=0;j<Taille(this.coord[i]);j++){
-      grid[i+this.x][this.coord[i][j]+this.y][0] = false;
-      grid[i+this.x][this.coord[i][j]+this.y][1] = undefined;
-  }
-  }
-};
-
-/** Methode Rotate de l'objet Block
- *   Permet de mettre le tetromino en rotation
- *   @return void
-**/
-Block.prototype.rotate = function(){  this.remove();
-  this.rotation = (this.rotation==3)?0:this.rotation+1;
-  this.coord    = tetros[this.type][this.rotation];
-  this.draw();
-};
-
-/** Methode Rotate de l'objet Block
- *   Permet de mettre le tetromino en rotation
- *   @return void
-**/
-Block.prototype.down = function(){
-  for(var i=0;i<Taille(this.coord);i++){
-    for(var j=0;j<Taille(this.coord[i]);j++){
-      next_empty = (grid[i+this.x+1]===undefined)?true:grid[i+this.x+1][this.coord[i][j]+this.y][0];
-      Ecrire(next_empty);
-      if(next_empty == true){
-        Ecrire("break");
-         this.fixe();
-         return false;
-      }
-  }
-  }
-  this.remove();
-  this.x++;
-  this.draw();
-};
-
-Block.prototype.move = function(sens){
-  for(var i=0;i<Taille(this.coord);i++){
-    for(var j=0;j<Taille(this.coord[i]);j++){
-      next_empty = (grid[i+this.x][this.coord[i][j]+this.y+sens]===undefined)?true:grid[i+this.x][this.coord[i][j]+this.y+sens][0];
-      Ecrire(next_empty);
-      if(next_empty == true){
-        Ecrire("Imposible");
-         return false;
-      }
-  }
-  }
-  this.remove();
-  this.y += sens;
-  this.draw();
-};
-
 /* Creation de la grille */
 var grid = Tableau(20, 10);
 for(var i=0;i<Taille(grid);i++){ // On initialise le tableau 
@@ -170,6 +47,7 @@ Rectangle(bord_next, bord, 5*c, 3*c);
 function run(){
  block = null;
  block = new Block(Hasard(7), bord, bord);
+ block.draw();
  time = null;
  time = setInterval(function() { interval(block); }, 1000);
 }
