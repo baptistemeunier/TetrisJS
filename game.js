efb7d1b1eb14f2dc6b33cf561fe7ef287675e837
FindYourSelf <baptiste.meunier0@gmail.com>
2015-10-23 00:33:41 +0200

    Ajout de l'afichage du prochain tetro

--- game.js
+++ game.js
@@ -3,18 +3,23 @@ var color  = [ "cyan", "yellow", "purple", "orange", "blue", "red", "lime"];
 var c = 30;   // Coté d'un block en pixel 
 var bord =20; // Position du bord gauche en pixel
 var bord_next = 3*bord+10*c; // Position du bord gauche cadre next
+var block;
+var blockNext = Hasard(7);
+var time;
+
 includeFile("blockObjet.js");
-document.onkeyup = function(event){ 
+window.onkeyup = function(event){ 
   switch(event.keyCode){
       case 16:
     Ecrire("Touche MAJ : Rotation gauche (16)");
+    block.rotate(-1);
       break;
 
       case 35:
         Ecrire("Touche Fin : Rotation droite (35)");
-        block.rotate();
+        block.rotate(1);
       break;
-      
+
       case 37:
         Ecrire("Touche <-  : Deplacement gauche (37)");
         block.move(-1);
@@ -24,7 +29,7 @@ document.onkeyup = function(event){
         Ecrire("Touche ->  : Deplacement droite (39)");
         block.move(1);
      break;
-     
+
       case 40:
         Ecrire("Touche BAS  : Deplacement Bas (40)");
         block.down();
@@ -34,7 +39,7 @@ document.onkeyup = function(event){
 
 /* Creation de la grille */
 var grid = Tableau(20, 10);
-for(var i=0;i<Taille(grid);i++){ // On initialise le tableau 
+for(var  i=0;i<Taille(grid);i++){ // On initialise le tableau 
   for(var j=0;j<Taille(grid[i]);j++){
   grid[i][j] = Tableau(2);
     grid[i][j][0] = false;
@@ -44,26 +49,42 @@ for(var i=0;i<Taille(grid);i++){ // On initialise le tableau
 
 Rectangle(bord-1, bord-1, 10*c+1, 20*c+1);
 Rectangle(bord_next, bord, 5*c, 3*c);
+
+function showNext(b){
+  var next = tetros[b][0];
+  Texte(bord_next, bord+5*c, next, "black");
+  RectanglePlein(bord_next+c/2, bord+c/2, 4*c, 2*c, "white");
+  for(var i=0;i<Taille(next);i++){
+    for(var j=0;j<Taille(next[i]);j++){
+      RectanglePlein(bord_next+c/2+next[i][j]*c, bord+c/2+i*c, c, c, color[b]);    
+    }
+  }
+}
+
 function run(){
  block = null;
- block = new Block(Hasard(7), bord, bord);
+ time = null; 
+ block = new Block(blockNext, bord, bord);
+ blockNext = Hasard(7);
  block.draw();
- time = null;
+ showNext(blockNext);
  time = setInterval(function() { interval(block); }, 1000);
 }
 function interval(b){
+  /*
   for(var i=0;i<Taille(grid);i++){
     var ret = "";
     for(var j=0;j<Taille(grid[i]);j++){
       ret += grid[i][j][0] + " : "; 
     }
     Ecrire(ret + " fin");
-  }
+  }*/
   if(b.down() == false){
     clearInterval(time);
     run();
   }
 }
-var block;
-var time;
-run();
+
+block = new Block(Hasard(7), bord, bord);
+showNext(blockNext);
+time = setInterval(function() { interval(block); }, 1000);
