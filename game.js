653997a3539d0c3b50aa28cd972c5aaf25336665
FindYourSelf <baptiste.meunier0@gmail.com>
2015-10-23 13:03:52 +0200

    Intégration de l'objet Graphic
    Reste encore quelque bug 
    
     et activation de fonction debug

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+setCanvasFont("Arial", "25px", "bold");
 var tetros = JSON.parse(readFile("data/tetrominos.json"));
 var color  = [ "cyan", "yellow", "purple", "orange", "blue", "red", "lime"];
 var c = 30;   // Coté d'un block en pixel 
@@ -7,31 +8,32 @@ var block;
 var blockNext = Hasard(7);
 var time;
 
-includeFile("blockObjet.js");
+includeFile("blockObject.js");
+includeFile("graphicObject.js");
 window.onkeyup = function(event){ 
   switch(event.keyCode){
       case 16:
-    Ecrire("Touche MAJ : Rotation gauche (16)");
+    //Ecrire("Touche MAJ : Rotation gauche (16)");
     block.rotate(-1);
       break;
 
       case 35:
-        Ecrire("Touche Fin : Rotation droite (35)");
+    //    Ecrire("Touche Fin : Rotation droite (35)");
         block.rotate(1);
       break;
 
       case 37:
-        Ecrire("Touche <-  : Deplacement gauche (37)");
+     //   Ecrire("Touche <-  : Deplacement gauche (37)");
         block.move(-1);
       break;
 
       case 39:
-        Ecrire("Touche ->  : Deplacement droite (39)");
+      //  Ecrire("Touche ->  : Deplacement droite (39)");
         block.move(1);
      break;
 
       case 40:
-        Ecrire("Touche BAS  : Deplacement Bas (40)");
+      //  Ecrire("Touche BAS  : Deplacement Bas (40)");
         block.down();
       break;
     }
@@ -47,19 +49,8 @@ for(var  i=0;i<Taille(grid);i++){ // On initialise le tableau
 }
 /* /Creation de la grille */
 
-Rectangle(bord-1, bord-1, 10*c+1, 20*c+1);
-Rectangle(bord_next, bord, 5*c, 3*c);
-
-function showNext(b){
-  var next = tetros[b][0];
-  Texte(bord_next, bord+5*c, next, "black");
-  RectanglePlein(bord_next+c/2, bord+c/2, 4*c, 2*c, "white");
-  for(var i=0;i<Taille(next);i++){
-    for(var j=0;j<Taille(next[i]);j++){
-      RectanglePlein(bord_next+c/2+next[i][j]*c, bord+c/2+i*c, c, c, color[b]);    
-    }
-  }
-}
+var Interface = new Graphic();
+Interface.initialiser();
 
 function run(){
  block = null;
@@ -67,18 +58,18 @@ function run(){
  block = new Block(blockNext, bord, bord);
  blockNext = Hasard(7);
  block.draw();
- showNext(blockNext);
+ Interface.drawNextBlock(blockNext);
  time = setInterval(function() { interval(block); }, 1000);
 }
 function interval(b){
-  /*
+  
   for(var i=0;i<Taille(grid);i++){
     var ret = "";
     for(var j=0;j<Taille(grid[i]);j++){
       ret += grid[i][j][0] + " : "; 
     }
     Ecrire(ret + " fin");
-  }*/
+  }
   if(b.down() == false){
     clearInterval(time);
     run();
@@ -86,5 +77,5 @@ function interval(b){
 }
 
 block = new Block(Hasard(7), bord, bord);
-showNext(blockNext);
+Interface.drawNextBlock(blockNext);
 time = setInterval(function() { interval(block); }, 1000);
