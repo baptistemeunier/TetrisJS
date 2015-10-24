0ab16c68b036e20b8b068dec8ff49c262260a404
baptistemeunier <baptiste.meunier0@gmail.com>
2015-10-24 13:28:23 +0200

    Ajout de la sauvegarde de pieces

--- gameObject.js
+++ gameObject.js
@@ -14,8 +14,8 @@ function Game() {
   this.lineScore = 0; // Nombre de ligne detruite
   this.nextBlock = Hasard(7); // Id de la prochaine piéce
   this.grid = Tableau(20, 10); // Grille du plateau de jeu 
-  this.block = new Block(Hasard(7), 0, 0);
-
+  this.block = new Block(Hasard(7), 0, 0); // Block du jeu en cours
+  this.save = null; // Block mis en memoire
   /** Methode initialiser
    *   Crée la grille est lance la partie 
    *   @return void
@@ -29,36 +29,54 @@ function Game() {
       }
     }
   };
-  
-  this.control = function(key){
-    switch (key) {
-      case 16:
-        //Touche MAJ : Rotation 90° gauche
-        this.grid = this.block.rotate(this.grid, -1);
-        this.debugGrid();
-        break;
-      case 35:
-        // Touche Fin : Rotation 90° droite
-        this.grid = this.block.rotate(this.grid, 1);
-        this.debugGrid();
-        break;
-      case 37:
-        // Touche <- : Deplacement gauche
-        this.grid = this.block.move(this.grid, -1);
-        this.debugGrid();
 
-        break;
-      case 39:
-        // Touche -> : Deplacement droite
-        this.grid = this.block.move(this.grid, 1);
-        this.debugGrid();
-        break;
-      case 40:
-        // Touche BAS : Deplacement bas (soft drop)
-        this.grid = this.block.down(this.grid);
-        this.debugGrid();
-        break;
-      }
+  this.control = function(key) {
+    switch (key) {
+    case 16:
+      //Touche MAJ : Rotation 90° gauche
+      this.grid = this.block.rotate(this.grid, -1);
+      break;
+    case 35:
+      // Touche Fin : Rotation 90° droite
+      this.grid = this.block.rotate(this.grid, 1);
+      break;
+    case 37:
+      // Touche <- : Deplacement gauche
+      this.grid = this.block.move(this.grid, -1);
+      break;
+    case 39:
+      // Touche -> : Deplacement droite
+      this.grid = this.block.move(this.grid, 1);
+      break;
+    case 40:
+      // Touche BAS : Deplacement bas (soft drop)
+      this.grid = this.block.down(this.grid);
+      break;
+    case 32:
+      // Touche ESPACE : Save block
+      this.saveBlock();
+      break;
+    }
+  };
+  this.saveBlock = function() {
+    if (this.save == this.block.type) {
+      return false;
+    }
+    Graphic.changeSave(this.block.type);
+    clearInterval(routine);
+    this.grid = this.block.remove(this.grid);
+    if (this.save == null) {
+      this.save = this.block.type;
+      return this.next();
+    }
+    var save = this.block.type;
+    this.block = null;
+    routine = null;
+    this.block = new Block(this.save, 0, 0);
+    this.save = save;
+    routine = setInterval(function() {
+      interval();
+    }, 1000);
   };
   this.start = function() {
     this.grid = this.block.draw(this.grid);
@@ -75,10 +93,10 @@ function Game() {
     routine = null;
     this.block = new Block(this.nextBlock, 0, 0);
     this.nextBlock = Hasard(7);
-    Graphic.drawNextBlock(this.nextBlock);    
-    if(this.checkLose() == true){
-		Graphic.end();
-		return false;
+    Graphic.drawNextBlock(this.nextBlock);
+    if (this.checkLose() == true) {
+      Graphic.end();
+      return false;
     }
     routine = setInterval(function() {
       interval();
@@ -103,38 +121,38 @@ function Game() {
       Ecrire(ret + " fin");
     }
   };
-  
-  this.checkLine = function(){
+
+  this.checkLine = function() {
     for (var i = 0; i < Taille(Game.grid); i++) {
       var complete = true;
       for (var j = 0; j < Taille(Game.grid[i]); j++) {
-        if(Game.grid[i][j][0] == false){
-			complete = false;
+        if (Game.grid[i][j][0] == false) {
+          complete = false;
         }
       }
-      if(complete == true){
-        for (var l=i; l > 0; l--) {
-          if(l != 0){
-              Game.grid[l] = Game.grid[l-1];
-          }else{
-			for(var j = 0; j < Taille(this.grid[l]); j++){
-				this.grid[l][j][0] = false;
-				this.grid[l][j][1] = undefined;
+      if (complete == true) {
+        for (var l = i; l > 0; l--) {
+          if (l != 0) {
+            Game.grid[l] = Game.grid[l - 1];
+          } else {
+            for (var j = 0; j < Taille(this.grid[l]); j++) {
+              this.grid[l][j][0] = false;
+              this.grid[l][j][1] = undefined;
             }
           }
         }
-        this.score += 100; 
+        this.score += 100;
         this.lineScore++;
         Graphic.drawInfo(this.level, this.score, this.lineScore);
       }
     }
   };
-  
-  this.checkLose = function(){
+
+  this.checkLose = function() {
     for (var i = 0; i < Taille(Game.grid[0]); i++) {
-      if(Game.grid[0][i][0] == true){
+      if (Game.grid[0][i][0] == true) {
         return true;
-      }  
+      }
     }
     return false;
   };
