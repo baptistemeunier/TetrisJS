1aeb77515afc07ec341d4b6111211f47fce4485f
baptistemeunier <baptiste.meunier0@gmail.com>
2015-11-11 15:42:46 +0100

    Ajout du pseudo en debut de partie

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ includeFile("gameObject.js");
 
 /* Initialisation des variables global */
 setCanvasFont("Arial", "15px", "bold"); // Selection de la police
+var bestScores = JSON.parse(readFile("data/score.json")); // Liste de pieces
 var tetros = JSON.parse(readFile("data/tetrominos.json")); // Liste de pieces
 var color = ["cyan", "yellow", "purple", "orange", "blue", "red", "lime"]; // Couleur des pièces
 var Graphic = new Graphic(); // Instance de l'objet Graphic
@@ -13,12 +14,12 @@ var routine; // ...
 var stop = false;
 
 window.onkeyup = function(event) { // Lorsque qu'une touche est relaché
-	if(event.keyCode == 27){
-		Game.pause();
-    }
+  if (event.keyCode == 27) {
+    Game.pause();
+  }
 };
 window.onkeydown = function(event) { // Lorsque qu'une touche est relaché
-	Game.control(event.keyCode);
+  Game.control(event.keyCode);
 };
 
 function interval() {
@@ -30,8 +31,7 @@ function interval() {
     Game.next();
   }
 }
-
-/* On initialise les Objets nesessaire à la partie */
+pseudo = Saisie("Merci de saisir votre pseudo"); /* On initialise les Objets nesessaire à la partie */
 Game.initialiser();
 Graphic.initialiser();
 
